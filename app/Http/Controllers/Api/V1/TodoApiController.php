02038f37cbb55157fa934b76c91ba387331a703d
Ivar <63834@gymnasium.ax>
2025-10-22 09:41:06 +0300

    lagt till funktioner för ändrig av uppgifter api

--- app/Http/Controllers/Api/V1/TodoApiController.php
+++ app/Http/Controllers/Api/V1/TodoApiController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Uppgift;
 use App\Repositories\Interfaces\UppgiftRepo;
+use Illuminate\Http\Request;
 
 class TodoApiController extends Controller
 {
+
     public function __construct(private UppgiftRepo $repo)
     {
 
@@ -16,5 +19,42 @@ class TodoApiController extends Controller
     {
         $lista = $this->repo->all();
         return response()->json(['todo' => $lista]);
+    }
+
+    public function get(Request $request)
+    {
+        $item = $this->repo->get($request->route('id'));
+        return response()->json(['todo' => $item]);
+    }
+
+    public function add(Request $request)
+    {
+
+        $text = $request->input('uppgift');
+        $uppgift = Uppgift::factory()->make(['text' => $text]);
+
+        $this->repo->add($uppgift);
+        return response()->json(['todo' => $uppgift], 201);
+    }
+    public function update(Request $request)
+    {
+        $id = filter_var($request->route('id'), FILTER_VALIDATE_INT);
+        $uppgift = $this->repo->get($id);
+
+        $uppgift->text = $request->input('uppgift');
+        $uppgift->done = $request->input('done', $uppgift->done);
+
+
+        $this->repo->update($uppgift);
+        return response()->json(['todo' => $uppgift]);
+
+    }
+    public function remove(Request $request)
+    {
+        $id = $request->request->get('uppgift');
+        $this->repo->delete($id);
+        return response()->json(['todo' => null]);
+
+
     }
 }
